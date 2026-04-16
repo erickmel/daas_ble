6e89a6a39727a926f8ab7c3e37cb338120ca82ef
Erick <melian.erick@gmail.com>
2026-04-16 20:19:10 +0200

    multi device

--- include/daas_nodes.h
+++ include/daas_nodes.h
@@ -5,7 +5,7 @@
 #define remote_uri "1ab2ddff"
 #elifdef NODEB
 #define localDIN 130
-#define remoteDIN 120
+#define remoteDIN 102
 #define local_uri "dcba4321"
-#define remote_uri "abcd1234"
+#define remote_uri "1ab2ddff"
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,12 @@
 #include "daas_nodes.h"
 #include "datatypes.h"
 
-bool nodeConnected = true;
+bool nodeConnected = false;
+bool requestedLogs = false;
 din_t foundDIN;
+
+const TickType_t periodStatus = pdMS_TO_TICKS(10000);
+TickType_t lastSend = 0;
 
 class event_daas : public IDaasApiEvent {
 public:
@@ -18,6 +22,7 @@ public:
     }
     void ddoReceived(int payload_size, typeset_t typeset, din_t din) override {
         printf("DDO Received\n");
+        if(typeset == 3410) requestedLogs = true;
     }
     void frisbeeReceived(din_t) override {}
     void nodeStateReceived(din_t) override {}
@@ -41,6 +46,35 @@ DaasAPI daas(event);
 
 dev_stat_t status;
 #define STATUS_TS 3110
+#define LOG_TS 3400
+uint8_t rand_range(uint8_t min, uint8_t max)
+{
+    if (min > max) {
+        uint8_t tmp = min;
+        min = max;
+        max = tmp;
+    }
+
+    return (uint8_t)(min + (rand() % (max - min + 1)));
+}
+
+sensors_t generate_random_sensors() {
+    sensors_t s;
+
+    s.heat = (int8_t)rand_range(40, 50);
+
+    s.climate_temp = rand_range(25, 30);
+
+    s.climate_hum = (uint8_t)rand_range(30, 35);
+
+    s.rumor = (uint8_t)rand_range(5,15);
+
+    s.daylight = (uint8_t)rand_range(50,60);
+
+    s.presence = (uint8_t)1;
+
+    return s;
+}
 
 void sendData(dev_stat_t data){
     DDO out(STATUS_TS);
@@ -56,7 +90,26 @@ void sendData(dev_stat_t data){
     out.allocatePayload(packetSize);
     out.setPayload(buffer, packetSize);
 
-    daas_error_t res = daas.push(102, &out);
+    daas_error_t res = daas.push(foundDIN, &out);
+    if(res == ERROR_NONE) printf("[PUSH] Sent data");
+    else printf("[PUSH] Error %d",res);
+}
+
+void sendLogData(log_record_t data){
+    DDO out(LOG_TS);
+
+    // uint8_t buffer[1] = {data};
+
+    constexpr uint32_t packetSize = sizeof(log_record_t);
+
+    uint8_t buffer[packetSize];
+    memset(buffer, 0, packetSize);          // buffer pulito
+    memcpy(buffer, &data, packetSize);     // copia sicura
+
+    out.allocatePayload(packetSize);
+    out.setPayload(buffer, packetSize);
+
+    daas_error_t res = daas.push(foundDIN, &out);
     if(res == ERROR_NONE) printf("[PUSH] Sent data");
     else printf("[PUSH] Error %d",res);
 }
@@ -77,19 +130,45 @@ bool receiveData(void){
 
 void periodicTask(void *parameter){
     for(;;){
-        #ifdef NODEA
+        // #ifdef NODEA
         if(!nodeConnected) {
             // daas.discovery();
             printf("Discovery \n");
         }
-        #endif
-        if(nodeConnected){
-            printf("Connected\n");
-        sendData(status);
-        receiveData();}
-        vTaskDelay(pdMS_TO_TICKS(10000));
+        // #endif
+        TickType_t now = xTaskGetTickCount();
+        if(now-lastSend >= periodStatus){
+            if(nodeConnected){
+                printf("Connected\n");
+                sensors_t sensors = generate_random_sensors();
+                dev_stat_t data;
+                data.sensors = sensors;
+                data.devicetime = 1776268445;
+                data.source = 1;
+                data.charge = 80;
+                data.charge_status = 3;
+                data.memory = 10;
+                data.link = 3;
+                    
+                sendData(data);
+                receiveData();
+                lastSend = now;
+            }
+        }
+        if(requestedLogs){
+            for(int i=0; i<5; i++){
+                log_record_t record;
+                record.devicetime = 1776268445 + rand_range(10,20);
+                record.id = rand_range(1,10);
+                record.value = rand_range(1,10);
+                sendLogData(record);
+            }
+            requestedLogs = false;
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
+
 
 // In ESP-IDF non esistono setup() e loop(). Esiste solo app_main()
 extern "C" void app_main() {
@@ -126,10 +205,10 @@ extern "C" void app_main() {
     printf("DaaS API Build Info: %s\n", daas.getBuildInfo());
     printf("Available Drivers: %s\n", daas.listAvailableDrivers());
     
-    err = daas.map(remoteDIN, _LINK_BLE, remote_uri);
-    if(err == ERROR_NONE) printf("Remote DIN mapped\n");
-    err = daas.locate(remoteDIN);
-    if(err == ERROR_NONE) printf("Remote DIN located\n");
+    // err = daas.map(remoteDIN, _LINK_BLE, remote_uri);
+    // if(err == ERROR_NONE) printf("Remote DIN mapped\n");
+    // err = daas.locate(remoteDIN);
+    // if(err == ERROR_NONE) printf("Remote DIN located\n");
     // Al posto del loop(), crei un task o metti un delay infinito
     while (1) {
       daas.doPerform(PERFORM_CORE_NO_THREAD); // Esegui il core in modalità real-time
